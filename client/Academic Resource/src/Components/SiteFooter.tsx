1f5558fb43ad5a60f7c90e38135ebd7e4ceb62ab
Khushi <khushirustagi051@gmail.com>
2026-02-17 16:52:39 +0530

    Final Frontend Commit

--- client/Academic Resource/src/Components/SiteFooter.tsx
+++
@@ -1,9 +0,0 @@
-const SiteFooter = () => (
-  <footer className="py-8 px-6 text-center">
-    <p className="text-sm text-muted-foreground">
-      Made by students • Hackathon Project
-    </p>
-  </footer>
-);
-
-export default SiteFooter
